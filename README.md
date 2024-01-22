50b67ef405698534a4f3fc421a5182ad2881393d
龙也 <yanchi.zyc@alibaba-inc.com>
2024-01-22 17:37:09 +0800

    🎉 Begin a project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# coco-lang-booster-plugin
+
+适用于 intelliJ 平台的编程语言增强插件。
+
+计划支持的编程语言：
+
+- golang
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "coco-lang-booster-plugin"
